236bec9b224d72e21dfc7b5d6763b06c18c48318
duman <duman200354@mail.ru>
2022-11-11 23:21:05 +0600

    db connected

---
+++ server/index.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+const app = express();
+dotenv.config();
+
+
+// CONSTANTS 
+const PORT = process.env.PORT || 5001;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_NAME = process.env.DB_NAME;
+
+
+// MIDDLEWARE
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({message: 'Fall in love'})
+})
+
+async function start() {
+  try {
+    await mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.ddjlrtl.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`)
+
+    app.listen(PORT, () => console.log('server on'));
+  } catch(error) {
+    console.log(error);
+  }
+}
+
+start()
