3e41666cf4055260b2c9c190ab85f5fdcbc6fe97
duman <duman200354@mail.ru>
2022-11-12 13:31:19 +0600

    front navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,28 @@
 import React from "react";
+import { Routes, Route } from 'react-router-dom'
+import Layout from "./components/Layout";
+import MainPage from './pages/MainPage'
+import PostsPage from './pages/PostsPage'
+import PostPage from './pages/PostPage'
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+import AddPostPage from "./pages/AddPostPage";
+import EditPostPage from "./pages/EditPostPage";
 
 function App() {
   return (
-    <div className="p-10 bg-black">
-     
-    </div>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<MainPage/>} />
+        <Route path="/register" element={<RegisterPage/>} />
+        <Route path="/login" element={<LoginPage/>} />
+        <Route path="/posts" element={<PostsPage/>} />
+        <Route path=':id' element={<PostPage/>} />
+        <Route path='/new' element={<AddPostPage/>} />
+        <Route path=':id/edit' element={<EditPostPage/>} />
+
+      </Routes>
+    </Layout>
   );
 }
 
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+
+const Navbar = () => {
+  const activeStyles = {
+    color: "white",
+  };
+
+  const isAuth = true;
+
+  return (
+    <div className="flex py-4 justify-between items-center">
+      <span className="flex justify-center items-center w-6 h-6 bg-gray-600 text-sm text-white rounded-sm">
+        E
+      </span>
+      {isAuth && (
+        <ul className="flex gap-8">
+          <li>
+            <NavLink
+              to={"/"}
+              className="text-xs text-gray-400 hover:text-white"
+              style={({ isActive }) => (isActive ? activeStyles : undefined)}
+              href="/"
+            >
+              Main
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"/posts"}
+              className="text-xs text-gray-400 hover:text-white"
+              style={({ isActive }) => (isActive ? activeStyles : undefined)}
+              href="/"
+            >
+              My Posts
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"/new"}
+              className="text-xs text-gray-400 hover:text-white"
+              style={({ isActive }) => (isActive ? activeStyles : undefined)}
+              href="/"
+            >
+              Add Post
+            </NavLink>
+          </li>
+        </ul>
+      )}
+
+      <div className="flex justify-between items-center bg-gray-600 text-xs text-white rounded-sm px-4 py-2">
+        {isAuth ? <button>Log Out</button> : <Link to="/login">Log In</Link>}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
